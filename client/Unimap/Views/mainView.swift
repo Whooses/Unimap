06d386d7fec3dc8b87b08f46480d3342eddbc6aa
Tony Nguyen <nguyen.mh.tony@gmail.com>
2025-02-03 23:40:21 -0500

    Created folders and reorganized files into correct corresponding folder

--- client/Unimap/Views/mainView.swift
+++ client/Unimap/Views/mainView.swift
@@ -11,26 +11,26 @@ extension Color {
     init(hex: String) {
         let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         let scanner = Scanner(string: hex)
-        
+
         if hex.hasPrefix("#") {
             scanner.currentIndex = hex.index(after: hex.startIndex)
         }
-        
+
         var rgbValue: UInt64 = 0
         scanner.scanHexInt64(&rgbValue)
-        
+
         let red = Double((rgbValue >> 16) & 0xFF) / 255.0
         let green = Double((rgbValue >> 8) & 0xFF) / 255.0
         let blue = Double(rgbValue & 0xFF) / 255.0
-        
+
         self.init(red: red, green: green, blue: blue)
     }
 }
 
 
-struct ContentView: View {
+struct MainView: View {
     @StateObject private var authViewModel = AuthViewModel()
-    
+
     var body: some View {
         if authViewModel.isAuthenticated {
             AuthenticatedView(authViewModel: authViewModel)
@@ -42,6 +42,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
