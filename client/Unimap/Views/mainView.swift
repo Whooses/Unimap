8553532717f91bf729e9fda693e5d3f1c0425341
bhavyap1010 <bhavya.patel9@student.tdsb.on.ca>
2025-04-29 17:38:52 -0400

    reverting to developing version for front end

---
+++ client/Unimap/Views/mainView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Unimap
+//
+//  Created by Bhavya Patel on 2025-01-22.
+//
+
+// UIImage+AverageColor.swift
+import SwiftUI
+
+struct MainView: View {
+    @StateObject private var authViewModel = AuthViewModel()
+
+    var body: some View {
+        // Auth model is currently set as true for development purpose
+        // It is suppose to stay false until getting verfied
+        // by the user.
+        if (authViewModel.isAuthenticated == true) {
+            AuthenticatedView(authViewModel: authViewModel)
+        }
+        else {
+            AuthenticatedView(authViewModel: authViewModel)
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+
+    }
+}
