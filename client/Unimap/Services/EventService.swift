c84fa2e969406836ed4c53b5e95b808dbcc26c5b
Tony Nguyen <nguyen.mh.tony@gmail.com>
2025-05-10 00:51:15 -0400

    Added doc comments for EventService and EventViewModel

--- client/Unimap/Services/EventService.swift
+++ client/Unimap/Services/EventService.swift
@@ -1,12 +1,18 @@
 import Foundation
 import SwiftUI
 
-
+/// Service responsible for fetching event data from the network.
 class EventService {
 
+    /// Handles network requests and responses.
     private let networkService = NetworkService()
 
-    func getEvents(from request: URLRequest) async throws -> [Event]{
+    /// Fetches an array of `Event` objects from the provided URL request.
+    ///
+    /// - Parameter request: The URLRequest specifying the endpoint for events.
+    /// - Returns: An array of `Event` objects.
+    /// - Throws: An error if the network request fails or decoding is unsuccessful.
+    func getEvents(from request: URLRequest) async throws -> [Event] {
         do {
             let data: [Event] = try await self.networkService.sendRequest(from: request, type: Event.self)
             return data
