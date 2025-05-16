98ea4dcabee451a308227fee0f5a070c253cfebf
Tony Nguyen <nguyen.mh.tony@gmail.com>
2025-05-16 18:13:09 -0400

    Added setPath for EventRequesetBuilder

--- client/Unimap/Utilities/URLBuilder.swift
+++ client/Unimap/Utilities/URLBuilder.swift
@@ -1,12 +1,11 @@
 import Foundation
 
-struct EventRequestBuilder {
-    
+class EventRequestBuilder: ObservableObject {
     enum Path: String {
         case events = "/events"
     }
     
-    enum SortOption: String {
+    enum Sort: String {
         case latest = "latest"
         case upcoming = "upcomming"
         case recentlyAdded = "recently added"
@@ -23,13 +22,8 @@ struct EventRequestBuilder {
     private var components: URLComponents
     private var queryItems: [URLQueryItem] = []
     
-    private static let dateFormatter: DateFormatter = {
-        let fmt = DateFormatter()
-        fmt.dateFormat = "yyyy-MM-dd"
-        return fmt
-    }()
     
-    // MARK: - Initx
+    // MARK: - Init
     
     init() {
         components = URLComponents()
@@ -40,40 +34,53 @@ struct EventRequestBuilder {
     
     // MARK: - Builder Methods
     
-    @discardableResult
-    mutating func setPath(_ path: Path) -> Self {
+    func setPath(_ path: Path) -> Self {
         if case .events = path {
             components.path = Path.events.rawValue
         }
         return self
     }
     
-    @discardableResult
-    mutating func setSort(_ option: SortOption) -> Self {
-        queryItems.removeAll (where: { $0.name == "sort" })
-        queryItems.append(URLQueryItem(name: "sort", value: option.rawValue))
+    func setSearch(_ text: String) -> Self {
+        queryItems.removeAll { $0.name == "search" }
+        queryItems.append(URLQueryItem(name: "search", value: text))
         return self
     }
     
-    @discardableResult
-    mutating func setClubs(_ clubs: [Club]) -> Self {
+    func setSort(_ option: Sort) -> Self {
+        queryItems.removeAll { $0.name == "sort" }
+        queryItems.append(URLQueryItem(name: "sort", value: option.rawValue))
+        return self
+    }
+
+    func setClubs(_ clubs: [Club]) -> Self {
         queryItems.removeAll { $0.name == "clubs" }
         let value = clubs.map(\.rawValue).joined(separator: ",")
         queryItems.append(URLQueryItem(name: "clubs", value: value))
         return self
     }
     
-    @discardableResult
-    mutating func setStartDate(_ date: Date) -> Self {
-        let value = Self.dateFormatter.string(from: date)
+    func setStartDate(_ date: Date) -> Self {
+        let dateFormatter: DateFormatter = {
+            let fmt = DateFormatter()
+            fmt.dateFormat = "yyyy-MM-dd"
+            return fmt
+        }()
+        
+        let value = dateFormatter.string(from: date)
         queryItems.removeAll { $0.name == "startDate" }
         queryItems.append(URLQueryItem(name: "startDate", value: value))
         return self
     }
     
-    @discardableResult
-    mutating func setEndDate(_ date: Date) -> Self {
-        let value = Self.dateFormatter.string(from: date)
+    func setEndDate(_ date: Date) -> Self {
+        let dateFormatter: DateFormatter = {
+            let fmt = DateFormatter()
+            fmt.dateFormat = "yyyy-MM-dd"
+            return fmt
+        }()
+        
+        let value = dateFormatter.string(from: date)
         queryItems.removeAll { $0.name == "endDate" }
         queryItems.append(URLQueryItem(name: "endDate", value: value))
         return self
