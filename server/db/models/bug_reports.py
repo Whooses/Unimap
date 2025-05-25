95f9b4f3a579061ce5a976de42561dda43d057a6
bhavyap1010 <bhavya.patel9@student.tdsb.on.ca>
2025-05-25 14:31:21 -0400

    added report system endpoints

---
+++ server/db/models/bug_reports.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime, String, func
+from sqlalchemy.orm import relationship
+from db.session import Base
+
+class BugReports(Base):
+    __tablename__ = "bug_reports"
+
+    id = Column(Integer, primary_key=True, index=True)
+    description = Column(Text, nullable=False)
+    page_url = Column(String, nullable=True)
+    screenshot_url = Column(String, nullable=True)
+    reporter_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    reporter = relationship("Users", back_populates="bug_reports")
---
+++ server/db/models/reports.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
+from db.session import Base
+
+class Reports(Base):
+    __tablename__ = "reports"
+
+    id = Column(Integer, primary_key=True, index=True)
+    reason = Column(Text, nullable=False)
+    event_id = Column(Integer, ForeignKey("events.id", ondelete="CASCADE"), nullable=False)
+    reporter_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    reporter = relationship("Users", back_populates="reports")
+    event = relationship("Events", back_populates="reports")
