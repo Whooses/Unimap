6ca8c331562652aab09fb819f22136ae5c39ab41
Tony Nguyen <nguyen.mh.tony@gmail.com>
2025-05-28 12:30:52 -0400

    Changes to only explore page to be ready for depployment

--- client/Unimap/Pages/AuthenticatedView/AuthenticatedView.swift
+++ client/Unimap/Pages/AuthenticatedView/AuthenticatedView.swift
@@ -4,26 +4,27 @@ struct AuthenticatedView: View {
     @ObservedObject var authViewModel: AuthViewModel
     
     var body: some View {
-        TabView {
-            HomePage()
-                .tabItem {
-                    Label("Home", systemImage: "house")
-                }
-            
-            ExplorePage()
-                .tabItem {
-                    Label("Explore", systemImage: "magnifyingglass")
-                }
-            
-            NotificationsPage()
-                .tabItem {
-                    Label("Notification", systemImage: "bell")
-                }
-            
-            ProfilePage()
-                .tabItem {
-                    Label("Profile", systemImage: "person.crop.circle")
-                }
-        }
+        ExplorePage()
+//        TabView {
+//            HomePage()
+//                .tabItem {
+//                    Label("Home", systemImage: "house")
+//                }
+//            
+//            ExplorePage()
+//                .tabItem {
+//                    Label("Explore", systemImage: "magnifyingglass")
+//                }
+//            
+//            NotificationsPage()
+//                .tabItem {
+//                    Label("Notification", systemImage: "bell")
+//                }
+//            
+//            ProfilePage()
+//                .tabItem {
+//                    Label("Profile", systemImage: "person.crop.circle")
+//                }
+//        }
     }
 }
