cb80325bdd27e4f5ed9caecae143fce78df22105
Tony Nguyen <nguyen.mh.tony@gmail.com>
2025-06-04 00:06:58 -0400

    Set up a fully functional CICD pipeline with Github Action

--- server/dependencies/event_dep.py
+++ server/dependencies/event_dep.py
@@ -3,17 +3,12 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .database_dep import get_db
-from services.event import mock_event_service, event_service
-from services.event.protocol_event_service import ProtocolEventService
+from services.event.event_service import EventService
 
 async def get_event_service(
     db: AsyncSession = Depends(get_db),
-) -> ProtocolEventService:
+) -> EventService:
     """
     Dynamically import and return the event service class based on the environment.
     """
-    env = os.getenv("ENV", "development")
-    if env == "production":
-        return event_service.EventService(db)
-    else:
-        return mock_event_service.MockEventService()
+    return EventService(db)
